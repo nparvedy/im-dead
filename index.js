5e2f56ef5c1def4f3157e1c26a4f2e33d6e29d31
NParvedy <nicolasparvedy@gmail.com>
2019-07-29 23:16:38 +0200

    JS mot au hasard + Split

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Partie CSS 
+
 const eltDivleft = document.querySelector('#left'); // On modifie le CSS du conteneur #left
 eltDivleft.style.display = "flex";
 eltDivleft.style.flexDirection = "column";
@@ -5,7 +7,7 @@ eltDivleft.style.justifyContent = "center";
 eltDivleft.style.alignItems = "center";
 
 const eltDivleftP = document.querySelectorAll("#left p"); // css de toutes les balises p
-eltDivleftP[0].innerHTML = "Indication : ";
+eltDivleftP[0].innerText = "Indication : ";
 
 
 const buttonDivLeft = document.querySelector('#left button'); // css du bouton 
@@ -13,3 +15,16 @@ buttonDivLeft.style.padding = "10px";
 buttonDivLeft.style.width = "70%";
 buttonDivLeft.style.color = "White";
 buttonDivLeft.style.backgroundColor = "black";
+buttonDivLeft.style.fontSize = "17px";
+buttonDivLeft.style.fontWeight = "bold";
+buttonDivLeft.style.borderRadius = "10px";
+
+// Partie Variable
+
+var tableauMot = ["Arbre", "Cuillere", "Table", "Javascript"];
+var motRandom = Math.floor(Math.random() * tableauMot.length);
+var motRandom = tableauMot[motRandom];
+var motRandom = motRandom.split('');
+var motATrouver = [''];
+
+console.log(motRandom);
