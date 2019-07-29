70badbc9580e3e8b1686cc01b1152f51425afe20
NParvedy <nicolasparvedy@gmail.com>
2019-07-29 22:53:19 +0200

    Ajout css en JS

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const elt = document.getElementById("left");
-elt.children[2] = "Salut les geeks";
+const eltDivleft = document.querySelector('#left'); // On modifie le CSS du conteneur #left
+eltDivleft.style.display = "flex";
+eltDivleft.style.flexDirection = "column";
+eltDivleft.style.justifyContent = "center";
+eltDivleft.style.alignItems = "center";
+
+const eltDivleftP = document.querySelectorAll("#left p"); // css de toutes les balises p
+eltDivleftP[0].innerHTML = "Indication : ";
+
+
+const buttonDivLeft = document.querySelector('#left button'); // css du bouton 
+buttonDivLeft.style.padding = "10px";
+buttonDivLeft.style.width = "70%";
+buttonDivLeft.style.color = "White";
+buttonDivLeft.style.backgroundColor = "black";
